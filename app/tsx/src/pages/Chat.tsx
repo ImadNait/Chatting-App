c0a8c0d730ec6da576dedcc7ae19d4fe3a4ed77a
Imad <naitmihoubimzd@gmail.com>
2024-11-02 11:44:42 +0100

    Update Chat.tsx

--- app/tsx/src/pages/Chat.tsx
+++ app/tsx/src/pages/Chat.tsx
@@ -9,7 +9,7 @@ export default function Chat(){
         <form id="form">
         <label htmlFor="message-input">Message</label>
         <input type="text" name="" id="msgIn" placeholder="send something" />
-        <button id="sendButton">Send</button>
+        <button id="sendButton">Send</button><br>
         <label htmlFor="room-input">Room</label>
         <input type="text" id="roomIn"  placeholder="join someone"/>
         <button id="roomButton">Join</button>
