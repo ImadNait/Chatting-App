8e7b86e216782988c7e83fa6d208bf00322910a9
Imad NaitMihoub <naitmihoubimzd@gmail.com>
2024-11-02 17:00:44 +0000

    commit

--- app/server/dist/server.js
+++ app/server/dist/server.js
@@ -4,23 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const ws_1 = require("ws");
 const dotenv_1 = require("dotenv");
+const users_1 = __importDefault(require("./routes/users"));
 (0, dotenv_1.config)();
 const wsPort = process.env.WS_PORT;
 const PORT = process.env.PORT;
 const app = (0, express_1.default)();
 console.log(wsPort);
+app.use('/users', users_1.default);
 app.use(express_1.default.json());
-const server = new ws_1.Server({ port: 6000 });
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.send('wsup niggaaa');
 });
-app.get('/home', (req, res) => {
+app.get('/home', (req, res, next) => {
     res.send('WELCOME HOME!!');
 });
-app.get('/about', (req, res) => {
+app.get('/about', (req, res, next) => {
     res.send('NOOO!!!');
+});
+app.get('/contacts', (req, res, next) => {
+    res.send('CONTACT US');
 });
 app.listen(PORT, () => {
     console.log(`Server runnin on port ${PORT}`);
--- app/tsx/src/pages/Chat.tsx
+++ app/tsx/src/pages/Chat.tsx
@@ -1,18 +1,43 @@
 
 
 export default function Chat(){
+    const joinButton = document.getElementById('roomButton') as HTMLButtonElement
+    const messageInp = document.getElementById('msgIn') as HTMLInputElement
+    const roomInp = document.getElementById('roomIn') as HTMLInputElement
+    const form = document.getElementById('form') as HTMLFormElement
 
 
+
+
+    form?.addEventListener("submit", e=>{
+        e.preventDefault()
+        const message = messageInp.value
+        const room = roomInp.value
+        if(message==="") return displayMsg(message)
+    })
+
+    joinButton.addEventListener("click", ()=>{
+        const room = roomInp.value
+    })
+    function displayMsg(message=messageInp.value) {
+        const div = document.createElement('div')
+        div.textContent = message
+        document.getElementById('message-cont')?.append(div)
+
+    }
 
     return(<>
+    <div className="app">
         <div id="message-cont"></div>
         <form id="form">
         <label htmlFor="message-input">Message</label>
         <input type="text" name="" id="msgIn" placeholder="send something" />
         <button id="sendButton">Send</button>
+        <br />
         <label htmlFor="room-input">Room</label>
         <input type="text" id="roomIn"  placeholder="join someone"/>
         <button id="roomButton">Join</button>
         </form>
+    </div>    
     </>);
 }
